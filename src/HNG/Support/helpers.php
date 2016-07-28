a928b979d9d30d48ed22ddf20fff0924dbbe26dc
Neo Ighodaro <neo@creativitykills.co>
2016-07-28 17:01:27 +0100

    Added helper function to create a new request quickly.

---
+++ src/HNG/Support/helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+use HNG\Http;
+
+if ( ! function_exists('crsRequest'))
+{
+    /**
+     * Create a new Request instance using the guzzle driver.
+     *
+     * @param  array  $config
+     * @return Http\Request
+     */
+    function crsRequest(array $config)
+    {
+        if (strtolower($driver) === 'guzzle') {
+            $driver  = new Http\GuzzleRequest([
+                'base_url' => $config['base_url']
+            ]);
+        } else {
+            throw new Exception("Unsupported request driver.");
+        }
+
+        return new Http\Request($driver, $config);
+    }
+}
