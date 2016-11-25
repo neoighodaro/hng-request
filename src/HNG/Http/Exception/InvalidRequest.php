ec30bad17228f171325487ecfdc6253216ea6ce1
Neo Ighodaro <neo@creativitykills.co>
2016-11-25 12:19:56 +0100

    Extended the exception to allow response passed

--- src/HNG/Http/Exception/InvalidRequest.php
+++ src/HNG/Http/Exception/InvalidRequest.php
@@ -2,4 +2,19 @@
 
 namespace HNG\Http\Exception;
 
-class InvalidRequest extends \Exception {}
+class InvalidRequest extends \Exception {
+
+    protected $response;
+
+    public function __construct($response, $message = null, $code = 0, Exception $previous = null)
+    {
+        $this->response = $response;
+
+        return parent::__construct($message, $code, $previous);
+    }
+
+    public function getRequestResponse()
+    {
+        return $this->response;
+    }
+}
